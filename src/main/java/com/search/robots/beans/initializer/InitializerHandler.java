af3e3104305fa851ab0920bc86ca5657cca3eee9
zyred <zhangyonghong@aitici.cn>
2025-11-20 22:01:25 +0800

    commit

--- src/main/java/com/search/robots/beans/initializer/InitializerHandler.java
+++ src/main/java/com/search/robots/beans/initializer/InitializerHandler.java
@@ -1,12 +1,16 @@
 package com.search.robots.beans.initializer;
 
 import com.search.robots.config.BotProperties;
+import com.search.robots.database.entity.Included;
 import com.search.robots.database.service.ConfigService;
+import com.search.robots.database.service.IncludedService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.User;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -18,13 +22,21 @@ import javax.annotation.Resource;
  */
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class InitializerHandler {
 
-    @Resource private BotProperties properties;
-    @Resource private ConfigService configService;
+    private final BotProperties properties;
+    private final ConfigService configService;
+    private final IncludedService includedService;
+
 
 
     public void init (User user) {
         this.configService.queryConfig();
+
+        List<Included> includedList = this.includedService.list();
+        for (Included included : includedList) {
+            included.updateEveryAdv();
+        }
     }
 }
